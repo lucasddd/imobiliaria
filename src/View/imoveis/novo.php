930042c900846caae724a3ebffaa161b425e3f32
lucasddd <lucas_ferreira.1@hotmail.com>
2018-11-29 23:40:36 -0200

    Alteracao form para enviar fotos

--- src/View/imoveis/novo.php
+++ src/View/imoveis/novo.php
@@ -5,20 +5,8 @@
 <div class="col-md-12">
 	<h1 class="text-center text-success">Novo imóvel</h1>
 	<div class="col-md-8 offset-md-2 form-horizontal">
-		<form method="POST" action="/admin/cliente/salvar">
+		<form method="POST" action="/admin/cliente/salvar" accept-charset="UTF-8" enctype="multipart/form-data">
 			<div class="form-group">
-				<div class="form-group">
-					<div class="text-danger">
-						{% if(erros) %}
-						<div class="alert alert-danger">
-							<strong class="text-center">Seu formulário contem erros.</strong><br>
-							{% for erro in erros %}
-							<small><li>{{erro}}</li></small>
-							{% endfor %}
-						</div>
-						{% endif %}
-					</div>
-				</div>
 				<h4 class="text-center text-success">Informações do proprietário (locatário)</h4>
 				<input type="hidden" class="form-control" id="id" name="id" value="{{locatario.getId()}}">
 				<div class="input-group form-group">
@@ -39,6 +27,18 @@
 				</div>
 				<hr style="color: black">
 				<h4 class="text-center text-success">Informações do imóvel</h4>
+				<div class="form-group">
+					<div class="text-danger">
+						{% if(erros) %}
+						<div class="alert alert-danger">
+							<strong class="text-center">Seu formulário contem erros.</strong><br>
+							{% for erro in erros %}
+							<small><li>{{erro}}</li></small>
+							{% endfor %}
+						</div>
+						{% endif %}
+					</div>
+				</div>
 				<div class="input-group form-group">
 					<div class="input-group-prepend">
 						<span class="input-group-text">Endereço</span>
